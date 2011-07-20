6362feb0ea2100c6baf3f4cccdebe52e123e2dcf
r15ch13 <r15ch13+git@gmail.com>
2011-07-20 12:36:15 +0200

    Aufenthalt Klasse hinzugefuegt

---
+++ TR_Verwaltung/TR_Verwaltung/Model/Aufenthalt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TR_Verwaltung.Sonstiges;
+using System.Data.SqlServerCe;
+
+namespace TR_Verwaltung.Model
+{
+    public class Aufenthalt : Model
+    {
+        public Schueler Schueler { get; set; }
+        public Klasse Klasse { get; set; }
+        public Lehrer ULehrer { get; set; }
+        public Lehrer TRLehrer { get; set; }
+        public int Ueberweisungsnummer { get; set; }
+        
+
+        public override int Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
